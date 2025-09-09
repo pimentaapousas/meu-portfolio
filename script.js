02a41717cbaaf96e75892122a5e6c34d0881ae13
Diego Pousas <diegopousas@gmail.com>
2025-09-08 23:31:59 -0300

    feat: add-skills-on-js

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-const skils = document.getElementById('skill');
-skill.value = 'a'
+const skill = document.createElement('li');
+skill.classList.add('skill-badge');
+const skillsList = document.getElementById('skills-list');
+
+const skills = ['HTML', 'CSS', 'JavaScript', 'Git', 'GitHub', 'Node.js', 'VueJS', 'Mongo DB', 'Postman'];
+
+skills.forEach((item) => {
+  const skillItem = document.createElement('li');
+  skillItem.classList.add('skill-badge');
+  skillItem.innerText = item;
+  skillsList.appendChild(skillItem);
+  skillItem.addEventListener('click', (e) => {
+  showSkillInfo = true;
+  console.log(e.target)
+  });
+});
+
+const setSkillInfo = (name, description) => {
+  const skillName = document.getElementById('skill-name');
+  const skillDescription = document.getElementById('skill-description');
+  skillName.innerText = name;
+  skillDescription.innerText = description;
+};
+
+const skillInfo = document.getElementById('skill-info');
+skillInfo.addEventListener('click', () => {
+  showSkillInfo = false;
+});
+
+let showSkillInfo = false
